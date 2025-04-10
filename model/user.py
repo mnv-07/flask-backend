908bad69e42797df7b6f78b0b1535a2217568c39
mnv-07 <141222059+mnv-07@users.noreply.github.com>
2025-04-10 18:32:29 +0000

    Updates

--- model/user.py
+++ model/user.py
@@ -1,11 +1,11 @@
 class User:
-    def __init__(self, email, password_hash, unique_key=None):
+    def __init__(self, email, password_hash, unique_key=None, connected_to=None, pending_requests=None, is_connected=False):
         self.email = email
         self.password_hash = password_hash
         self.unique_key = unique_key
-        self.connected_to = None  # Email of the user this user is connected to
-        self.pending_requests = []  # List of emails who have sent connection requests
-        self.is_connected = False
+        self.connected_to = connected_to
+        self.pending_requests = pending_requests or []
+        self.is_connected = is_connected
 
     def add_pending_request(self, requester_email):
         if requester_email not in self.pending_requests:
